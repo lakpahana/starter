2b3af5e2d4c04e68cb283fdcc939e0dfc9009645
lakpahana <agslakpahana@gmail.com>
2026-01-10 00:30:12 +0530

    feat: add mini.animate plugin and update keymaps for terminal toggle

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+
+-- VSCODE like terminal keymaps
+-- Control backtick to toggle terminal
+-- map("n", "<leader>ft", function() Snacks.terminal(nil, { cwd = LazyVim.root() }) end, { desc = "Terminal (Root Dir)" })
+
+vim.keymap.set("n", "<C-`>", function() Snacks.terminal(nil, { cwd = LazyVim.root() }) end, { desc = "Terminal (Root Dir)" })
+--same command to close terminal
+vim.keymap.set("t", "<C-`>", function() Snacks.terminal(nil, { cwd = LazyVim.root() }) end, { desc = "Terminal (Root Dir)" })
